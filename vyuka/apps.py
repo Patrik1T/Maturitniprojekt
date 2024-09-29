14611a9c632413c7d467d8faa0de8c1d58594273
Patrik1T <lozsko@seznam.cz>
2024-09-29 23:07:17 +0200

    Pocatecniknostrukce
    Udělání readme.md, SQL a náčrt stránek a udělání základy v aplikaci.

--- vyuka/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class VyukaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'vyuka'
--- vyuka/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Material, Comment
-
-class MaterialForm(forms.ModelForm):
-    class Meta:
-        model = Material
-        fields = ['title', 'release_date', 'runtime', 'rate', 'predmet']
