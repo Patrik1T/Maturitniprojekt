06843ad8389ed62c153b5eab219d8a9cf1cdcd39
Patrik1T <lozsko@seznam.cz>
2024-12-20 17:54:23 +0100

    Dělání testů

--- .idea/revas/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from .views import LoginView
-from django.conf import settings
-from django.conf.urls.static import static
-urlpatterns = [
-    path('verify_code/', views.verify_code, name='verify_code'),
-    path('prihlasovacistranka/', LoginView.as_view(), name='prihlasovacistranka'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
